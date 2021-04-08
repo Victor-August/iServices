8d98047c35b2ed89837a247674480096c6aad9c9
Victor Alves <56544892+Victor-August@users.noreply.github.com>
2021-04-08 13:28:46 -0300

    Criando novo NavBar

--- src/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.css';
-import Splash from './pages/Splash/splash.jsx';
-import PageRoutes from './pages/pageRoutes';
-
-ReactDOM.render(<PageRoutes/> ,document.getElementById('root'));
-
