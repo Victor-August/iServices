942ea0d054dc1a25a97cd7a7009389a59a4664f5
Victor Alves <56544892+Victor-August@users.noreply.github.com>
2021-04-12 21:22:08 -0300

    Seção Services

--- src/components/InfoSection/InfoElements.js
+++ src/components/InfoSection/InfoElements.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const InfoContainer = styled.div`
     color: #fff;
-    background: ${({ lightBg }) => (lightBg ? '#f9f9f9' : '010606')};
+    background: ${({ lightBg }) => (lightBg ? '#f9f9f9' : '#010606')};
 
     @media screen and (max-width: 768px){
         padding: 100px 0;
@@ -25,7 +25,7 @@ export const InfoRow = styled.div`
     display: grid;
     grid-auto-columns: minmax(auto, 1fr);
     align-items: center;
-    grid-template-areas: ${({ imgStart }) => (imgStart ? `'col2 col1` : `'col1 col2'`)};
+    grid-template-areas: ${({ imgStart }) => (imgStart ? `'col2 col1'` : `'col1 col2'`)};
 
     @media screen and (max-width: 768px){
         grid-template-areas: ${({ imgStart }) => (imgStart ? `'col1' 'col2'` : `'col1 col1' 'col2 col2'`)};
@@ -50,7 +50,7 @@ export const TextWrapper = styled.div`
     padding-bottom: 60px;
 `;
 
-export const TopLine = styled.p`
+export const TopLine = styled.p` 
     color: #01bf71;
     font-size: 16px;
     line-height: 16px;
